b4dfc9a653a7e8ee002ca38d17d7703b7cf245df
johnson <johnsonhao@js.local>
2020-01-10 23:56:21 -0500

    enhence routing in views

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
 # Create your views here.
 from .models import BlogPost
 
 
 
-def blog_post_detail_page(request, slug):
-
-    # try:
-    queryset = BlogPost.objects.filter(slug = slug)
-    obj = queryset.first()
-    #obj = get_object_or_404(BlogPost, slug=slug)
-
-    template_name = 'blog_post_detail_page.html'
-    context = {"object": obj}
-    return render(request, template_name, context)
+def home_page(request):
+    qs = str(BlogPost.objects.count())
+    return HttpResponse("There are "+qs+" posts available")
 
 #for several objects
 def blog_post_list_view(request):
@@ -34,19 +27,22 @@ def blog_post_create_view(request):
     return render(request, template_name, context)
 
 #for 1 single object
-def blog_post_detail_view(request):
+def blog_post_detail_view(request, slug):
     #one object or the detail view
     obj = get_object_or_404(BlogPost, slug = slug)
     template_name = 'blog_post_detail.html'
-    context = {'object_': obj}
+    context = {'object': obj}
     return render(request, template_name, context)
 
-def blog_post_update_view(request):
+def blog_post_update_view(request, slug):
+    print(request)
+    obj = get_object_or_404(BlogPost, slug = slug)
     template_name = 'blog_post_update.html'
     context = {'object': obj, 'form': None}
     return render(request, template_name, context)
 
-def blog_post_delete_view(request):
+def blog_post_delete_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug = slug)
     template_name = 'blog_post_delete.html'
     context = {'object': obj}
     return render(request, template_name, context)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,28 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog.views import blog_post_create_view
 from .views import (
     home_page,
     about_page,
     contact_page,
     example,
     )
-from blog.views import (
-    blog_post_detail_page,
-    blog_post_list_view,
-    blog_post_create_view,
-    blog_post_detail_view,
-    blog_post_update_view,
-    blog_post_delete_view,
-    )
+
 
 urlpatterns = [
     path('', home_page),
+    path('blog/', include('blog.urls')),
+    path('blog-create/', blog_post_create_view),
+
     path('about/', about_page),
     path('contact/', contact_page),
     path('example/', example),
-    path('blog/', blog_post_list_view),
-    path('blog/<str:slug>', blog_post_detail_page),
+    #path('blog/<str:slug>', blog_post_detail_page),
     path('admin/', admin.site.urls),
 ]
